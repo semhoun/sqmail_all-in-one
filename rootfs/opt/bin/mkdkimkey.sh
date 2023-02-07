acf728f24212f3b93834314a99bc0c46f22ce3ad
Nathanaël Semhoun <nathanael@semhoun.net>
2023-02-07 14:36:56 +0100

    Fix for dkim mails

--- rootfs/opt/bin/mkdkimkey.sh
+++ rootfs/opt/bin/mkdkimkey.sh
@@ -1,8 +1,8 @@
 #********************************************************************************
 # Create/Handle domainkeys for openqmail/eQmail/(net)qmail and derivatives      #
 #                                                                               #
-# Author: Kai Peter (parts taken from Joerg Backschues), ©2014                 #
-# Version: 0.32 -> 0.42
+# Author: Kai Peter (parts taken from Joerg Backschues), ©2014                  #
+# Version: 0.32 -> 0.43                                                         #
 # Licence: This program is  Copyright(C) ©2015 Kai Peter.  It can be copied and #
 #          modified according to the GNU GENERAL PUBLIC LICENSE (GPL) Version 2 #
 #          or a later version. This software comes without any warranty.        #
@@ -15,11 +15,14 @@
 # b) RSA and Ed25519 private/public keys are considered (-> RFC 8463)           #
 # c) tinydns supports DKIM records while just providing the public key;         #
 #    beware of the 'selector'; it is set to 'default'                           #
+# d) Unlike previous versions, the new private key is *not* automatically       #
+#    linked to a file named 'default'; but rather to                            #
+#    rsa|ed25519.private_<selector> -> <selector>                               #
 #********************************************************************************
 DKDIR="/var/qmail/ssl/domainkeys"
 USR="qmailq"
 GRP="sqmail"
-MODULUS=1024
+MODULUS=2048
 CURVE=0
 VERBOSE=0
 SELECTOR="default"
@@ -32,14 +35,34 @@ if [ $? -ne 0 ] ; then
 fi
 
 showHelp() {
-echo "Usage:  $(basename $0) [-hpc] [-s selector] [-n modulus] domain"
+echo "Usage:  $(basename $0) [-hpc] [-s selector] [-m modulus] domain"
 echo
 echo "-h   (show this help and exit)"
 echo "-v   (verbose output)"
 echo "-p   (print TXT record for domain)"
 echo "-s   <selector> (set the selector)"
-echo "-m   <N> (set RSA modulo size; default: 1024 bits)"
-echo "-c   (generate Ed25519-sha256 keys)"
+echo "-m   <N> (set RSA modulo size; default: 2048 bits)"
+echo "-c   (generate Ed25519 keys)"
+echo ""
+echo "RSA key generation:"
+echo "=================="
+echo ""
+echo "     mkdkimkey -s rsa-selector domain"
+echo ""
+echo "Ed25519 key generation:"
+echo "======================"
+echo ""
+echo "     mkdkimkey -c -s ed-selector domain"
+echo ""
+echo "Key activation:"
+echo "=============="
+echo "     The created private key 'rsa|ed25519.private_<selector>'"
+echo "     is automatically symlinked to '<selector>'."
+echo "     If included in 'control/dkimdomains', this file name"
+echo "     is picked up by qmail-dksign for signing outgoing mails."
+echo "     The default name for '<selector>' is 'default'."
+echo "     This is implicitely assumed if no particular '<selector>' is given."
+echo ""
 exit 1 ; 
 }
 
@@ -87,7 +110,7 @@ if [ -f rsa.public_${SELECTOR} ]; then
 elif [ -f ed25519.public_${SELECTOR} ]; then
   echo "\"v=DKIM1; k=ed25519; t=y; p=`grep -v -e '^-' ed25519.public_${SELECTOR} | tr -d '\n'`\""
 fi
-echo -n "Default private key pointing to: "; ls -l default |  awk '{print $(NF-2), $(NF-1), $NF}'
+echo -n "You need to publish this TXT record in the DNS before activating [rsa|ed25519].private_${SELECTOR} -> ${SELECTOR} for signing."
 
 echo ; exit 0;
 }
@@ -132,7 +155,7 @@ if [ ${CURVE} -eq 0 ] ; then
   ${OPENSSL} genrsa -out rsa.private_${SELECTOR} ${MODULUS}
   ${OPENSSL} rsa -in rsa.private_${SELECTOR} \
              -out rsa.public_${SELECTOR} -pubout -outform PEM
-   ln -sf rsa.private_${SELECTOR} default
+   ln -sf rsa.private_${SELECTOR} ${SELECTOR}
 else
   if [ -f ed25519.public_${SELECTOR} ]; then
     cp ed25519.public_${SELECTOR}  ed25519.public_${SELECTOR}.previous
@@ -142,7 +165,7 @@ else
              -out ed25519.private_${SELECTOR}
   ${OPENSSL} pkey -in ed25519.private_${SELECTOR} \
              -out ed25519.public_${SELECTOR} -pubout 
-  ln -sf ed25519.private_${SELECTOR} default
+  ln -sf ed25519.private_${SELECTOR} ${SELECTOR}
 fi
 
 # Set permissions
